2672778e310de72f71fb5769fce7284df4567773
Alexandros Rekkas <a.rekkas@lumc.nl>
2018-06-18 10:47:53 +0200

    Make plotCovariateBalance.R faster

--- R/PlotCovariateBalance.R
+++ R/PlotCovariateBalance.R
@@ -9,6 +9,7 @@
 #' @param useStabilizedWeights Should stabilized weights be used?
 #' @param truncatedWeights Should truncated weights be used? If FALSE, then stabilized weights are used instead
 #' @param truncationQuantiles The quantiles to perform weight truncation
+#' @param showNotBalancedCovariateIds Show covariate ids that were not balanced after weighting?
 #'
 #' @return The covariate balance plot
 #' @export
@@ -21,7 +22,9 @@ plotCovariateBalance <- function(ps,
                                  weightsType = 'ATE',
                                  useStabilizedWeights = TRUE,
                                  truncatedWeights = TRUE,
-                                 truncationQuantiles = c(.01, .99)){
+                                 truncationQuantiles = c(.01, .99),
+                                 showNotBalancedCovariateIds = TRUE){
+
   if(calculateWeights)
     ps <- createIPW(ps,
                     weightsType = weightsType,
@@ -29,39 +32,52 @@ plotCovariateBalance <- function(ps,
                     truncatedWeights = truncatedWeights,
                     truncationQuantiles = truncationQuantiles)
 
-  nTreatmentRaw <- sum(ps$treatment)
-  nComparatorRaw <- sum(!ps$treatment)
-  nTreatmentWeighted <- sum(ps$treatment*ps$weights)
-  nComparatorWeighted <- sum((!ps$treatment)*ps$weights)
-  uniqueRowIds <- unique(cohortMethodData$covariates$rowId)
-  uniqueCovariateIdsFull <- unique(cohortMethodData$covariates$covariateId)
-  d <- data.frame(covariateId = numeric(),
-                  before = numeric(),
-                  after = numeric())
+  nTreatment <- sum(ps$treatment)
+  nComparator <- sum(!ps$treatment)
 
-  for(i in 1:length(uniqueCovariateIdsFull)){
+  stratumSubset <- ff::as.ffdf(cohortMethodData$covariates[cohortMethodData$covariates$rowId[] %in% ps$rowId, ]) # better way?
+  subsetPs <- subset(ps, select = c(rowId, treatment, weights))
+  mergedSubset <- ffbase::merge.ffdf(stratumSubset, ff::as.ffdf(subsetPs))
 
-    uniqueCovariateId <- uniqueCovariateIdsFull[i]
-    subsetCovariates <- subset(cohortMethodData$covariates, covariateId == uniqueCovariateId)
-    subsetPs <- subset(ps, rowId %in% subsetCovariates[]$rowId)
-    pHatTreatment <- sum(subsetPs$treatment)/nTreatmentRaw
-    pHatComparator <- sum(!subsetPs$treatment)/nComparatorRaw
-    beforeWeighting <- 100*abs((pHatTreatment - pHatComparator) /
-                                 sqrt((pHatTreatment*(1 - pHatComparator) + pHatComparator*(1 - pHatTreatment))/2))
-    pHatTreatment <- sum(subsetPs$treatment*subsetPs$weights)/nTreatmentWeighted
-    pHatComparator <- sum((!subsetPs$treatment)*subsetPs$weights)/nComparatorWeighted
-    afterWeighting <- 100*abs((pHatTreatment - pHatComparator) /
-                                sqrt((pHatTreatment*(1 - pHatComparator) + pHatComparator*(1 - pHatTreatment))/2))
-    d[i, ] <- c(uniqueCovariateId, beforeWeighting, afterWeighting)
+  sTreatment <- FeatureExtraction::bySumFf(mergedSubset$treatment*mergedSubset$covariateValue,
+                                           mergedSubset$covariateId)
+  sComparator <- FeatureExtraction::bySumFf((!mergedSubset$treatment)*mergedSubset$covariateValue,
+                                            mergedSubset$covariateId)
+  names(sTreatment) <- c('covariateId', 'sum')
+  names(sComparator) <- c('covariateId', 'sum')
+  pHatTreatment <- sTreatment$sum / nTreatment
+  pHatComparator <- sComparator$sum / nComparator
+  beforeWeighting <- 100*abs((pHatTreatment - pHatComparator) /
+                               sqrt((pHatTreatment*(1 - pHatComparator) + pHatComparator*(1 - pHatTreatment))/2))
 
-  }
-  d$inside <- ifelse((d$before >10 & d$after>10), d$covariateId, '')
-  ggplot2::ggplot(d, ggplot2::aes(before, after)) +
-    ggplot2::geom_point(size = 1) +
+  sTreatment <- FeatureExtraction::bySumFf(mergedSubset$treatment*mergedSubset$covariateValue*mergedSubset$weights,
+                                           mergedSubset$covariateId)
+  sComparator <- FeatureExtraction::bySumFf((!mergedSubset$treatment)*mergedSubset$covariateValue*mergedSubset$weights,
+                                            mergedSubset$covariateId)
+  names(sTreatment) <- c('covariateId', 'sum')
+  names(sComparator) <- c('covariateId', 'sum')
+  nTreatment <- sum(ps$treatment*ps$weights)
+  nComparator <- sum((!ps$treatment)*ps$weights)
+  pHatTreatment <- sTreatment$sum / nTreatment
+  pHatComparator <- sComparator$sum / nComparator
+  afterWeighting <- 100*abs((pHatTreatment - pHatComparator) /
+                              sqrt((pHatTreatment*(1 - pHatComparator) + pHatComparator*(1 - pHatTreatment))/2))
+
+  result <- data.frame(beforeWeighting,
+                       afterWeighting,
+                       covariateId = sTreatment$covariateId)
+  result$inside <- ifelse((result$beforeWeighting >10 & result$afterWeighting>10), result$covariateId, '')
+
+  p <- ggplot2::ggplot(result, ggplot2::aes(beforeWeighting, afterWeighting)) +
+    ggplot2::geom_point(size = 1, color = 'blue', alpha = .5) +
     ggplot2::geom_vline(ggplot2::aes(xintercept = 10), linetype = 'dashed', color = 'red') +
     ggplot2::geom_hline(ggplot2::aes(yintercept = 10, color = 'red'), linetype = 'dashed', color = 'red') +
-    ggplot2::geom_text(ggplot2::aes(label=inside), hjust=0, vjust=1.2) +
     ggplot2::xlab('Before weighting') +
     ggplot2::ylab('After weighting')
+  if(showNotBalancedCovariateIds)
+    p <- p + ggplot2::geom_text(ggplot2::aes(label=inside), hjust=0, vjust=1.2)
+
+  return(p)
+
 
 }
