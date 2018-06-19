0a030d0b93c7d5891ee27e8ec4a1ea6bc4057a05
Alexandros Rekkas <a.rekkas@lumc.nl>
2018-06-19 15:59:17 +0200

    Test absolute risk reduction function

--- R/RunRiskStratifiedEstimation.R
+++ R/RunRiskStratifiedEstimation.R
@@ -1,105 +1,126 @@
+library(doSNOW)
+library(ggplot2)
 
-
-runRiskStratifiedEstimation <- function(cohortMethodData,population, modelSettings, save,
+runRiskStratifiedEstimation <- function(cohortMethodData, population, modelSettings, save,
                                         testSplit = 'person', testFraction = .3, nfold = 10,
-                                        riskStrata = 4, excludeCovariateIds = NULL,
-                                        useStabilizedWeights = F, truncationQuantiles = c(.025, .975),
-                                        timePoint){
-
+                                        riskStrata = 4, weightsType = 'ATE', truncatedWeights = TRUE, 
+                                        useStabilizedWeights = FALSE, truncationQuantiles = c(.01, .99), 
+                                        timePoint, binary = TRUE, includeAllOutcomes = TRUE,
+                                        requireTimeAtRisk = TRUE, plpPlot = TRUE, psThreads = 1,
+                                        treatmentLabel = NULL, comparatorLabel = NULL){
+  
   #########################################
   # PREDICTION
   #########################################
-
+  
   plpData <- cmToPlpData(cohortMethodData)
-
-  subFolder <- 'TrainedModel'
+  
+  subFolder <- modelSettings$name
   outputFolder <- file.path(save, subFolder, fsep = '\\')
   dir.create(outputFolder, recursive = TRUE)
-
-  # Run the model ----
-  results <- PatientLevelPrediction::runPlp(
-    population = population, # Check if this will work using the same
+  populationCall <- attr(population, 'metaData')$call
+  
+  populationPlp <- 
+    PatientLevelPrediction::createStudyPopulation(plpData = plpData,
+                                                  outcomeId = populationCall$outcomeId,
+                                                  firstExposureOnly = populationCall$firstExposureOnly,
+                                                  washoutPeriod = populationCall$washoutPeriod,
+                                                  removeSubjectsWithPriorOutcome = populationCall$removeSubjectsWithPriorOutcome,
+                                                  priorOutcomeLookback = populationCall$priorOutcomeLookback,
+                                                  minTimeAtRisk = populationCall$minDaysAtRisk,
+                                                  riskWindowStart = populationCall$riskWindowStart,
+                                                  addExposureDaysToStart = populationCall$addExposureDaysToStart,
+                                                  riskWindowEnd = populationCall$riskWindowEnd,
+                                                  addExposureDaysToEnd = populationCall$addExposureDaysToEnd,
+                                                  binary = binary,
+                                                  includeAllOutcomes = includeAllOutcomes,
+                                                  requireTimeAtRisk = requireTimeAtRisk)
+  
+  # Run the prediction model ----
+  resultsPrediction <- PatientLevelPrediction::runPlp(
+    population = populationPlp, 
     plpData = plpData,
     modelSettings = modelSettings,
     testSplit = testSplit,
     testFraction = testFraction,
     nfold = nfold
   )
-
-  PatientLevelPrediction::savePlpModel(results$model, dirPath = file.path(outputFolder, "model", fsep = '\\'))
-  PatientLevelPrediction::savePlpResult(results, dirPath = file.path(outputFolder, 'lr', fsep = '\\'))
-
+  
+  PatientLevelPrediction::savePlpResult(resultsPrediction, dirPath = outputFolder)
+  
   # add plots and document to output folder
-  PatientLevelPrediction::plotPlp(
-    results,
-    file.path(outputFolder, 'plpmodels', results$analysisRef$analysisId, fsep = '\\')
-  )
-
-
+  if(plpPlot)
+    PatientLevelPrediction::plotPlp(resultsPrediction,
+      file.path(outputFolder, 'Plots', resultsPrediction$analysisRef$analysisId, fsep = '\\'))
+  
+  
   #########################################
   # RISK STRATIFICATION
   #########################################
-
+  
   # creates new column with stratum numbers -> stores the stratum number and the subjectId to mapMatrix
-  mapMatrix <- dplyr::mutate(results$prediction, riskStratum = dplyr::ntile(results$prediction$value, riskStrata))[, c(2, 14)]
-  saveRDS(mapMatrix, file.path(save, subFolder, 'mapMatrix.rds', fsep = '\\'))
-
-
+  mapMatrix <- dplyr::mutate(resultsPrediction$prediction, 
+                             riskStratum = dplyr::ntile(resultsPrediction$prediction$value, riskStrata))
+  mapMatrix <- subset(mapMatrix, select = c('subjectId', 'riskStratum'))
+  saveRDS(mapMatrix, file.path(save, 'mapMatrix.rds', fsep = '\\'))
+  
+  
   #########################################
   # RISK STRATIFIED ANALYSIS
   #########################################
 
-  #library(tictoc)
-  #library(doParallel)
-  #library(foreach)
-
-  cl <- makeCluster(riskStrata)
-  registerDoParallel(cl)
-
+  cl <- makeSOCKcluster(psThreads)
+  registerDoSNOW(cl)
+  
+  pb <- txtProgressBar(max = riskStrata, style=3)
+  progress <- function(n) setTxtProgressBar(pb, n)
+  opts <- list(progress = progress)
+  
   ps <- list()
-  tic()
-  ps <- foreach(i = 1:riskStrata) %dopar%{
-
-    cohortMethodData <- CohortMethod::loadCohortMethodData(file.path(save, 'Data\\cmData', fsep = '\\'))
-    mapMatrix <- readRDS(file.path(save, 'TrainedModel\\mapMatrix.rds', fsep = '\\'))
-    populationCm <- readRDS(file.path(save, 'populationCm.rds', fsep = '\\'))
-    populationRiskStratified <- populationCm[populationCm$subjectId %in% mapMatrix$subjectId[mapMatrix$riskStratum == i], ]
-    CohortMethod::createPs(cohortMethodData = cohortMethodData, population = populationRiskStratified,
-                           excludeCovariateIds = excludeCovariateIds)
-
+  tt <- Sys.time()
+  
+  ps <- foreach(i = 1:4, .options.snow = opts) %dopar%{
+    
+    populationRiskStratified <- population[population$subjectId %in% mapMatrix$subjectId[mapMatrix$riskStratum == i], ]
+    CohortMethod::createPs(cohortMethodData = cohortMethodData,
+                           population = populationRiskStratified,
+                           control = Cyclops::createControl(threads = -1,
+                                                            tolerance = 2e-07,
+                                                            cvRepetitions = 10,
+                                                            startingVariance = .01))
+    
   }
-  toc()
   stopCluster(cl)
-  saveRDS(ps, file.path(save, subFolder, 'psList.rds', fsep = '\\'))
+  close(pb)
+  
+  saveRDS(ps, file.path(save, 'psList.rds', fsep = '\\'))
+  
 
   #########################################
   # Weighted K-M curves/combined K-M plot
   #########################################
+  
 
-  psList <- readRDS(file.path(save, subFolder, 'psList.rds', fsep = '\\'))
-
-  subFolder <- file.path('TrainedModel', 'RiskStratifiedAnalysis', fsep = '\\')
-  directory <- file.path(mainFolder, subFolder,fsep = '\\')
-  dir.create(directory, recursive = TRUE)
-
-  plotsKM <- list()
+  dataKM <- list()
   for(i in 1:riskStrata){
 
-    ps <- psList[[i]]
-
-    plotsKM[[i]] <- weightedKM(ps,
-                               useSW = useStabilizedWeights,
-                               truncationQuantiles = truncationQuantiles,
-                               title = paste0('Risk Stratum ', i),
-                               legend.position = c(.2, .14))$plot
-
-    # pdf(file.path(directory, paste0('KMIPW_mine', i, '.pdf'), fsep = '\\'), onefile = FALSE)
-    # print(plotsKM[[i]])
-    # dev.off()
-
-    print(i)
-
+    dataKM[[i]] <- weightedKM(ps[[i]],
+                              weightsType = weightsType,
+                              truncatedWeights = truncatedWeights,
+                              useStabilizedWeights = useStabilizedWeights,
+                              truncationQuantiles = truncationQuantiles,
+                              treatmentLabel = treatmentLabel,
+                              comparatorLabel = comparatorLabel)
   }
+  
+  saveRDS(dataKM, file = file.path(save, 'dataKM.rds'))
+  
+  results <- list(ps = ps,
+                  dataKM = dataKM)
+  return(results)
+}
+
+
 
 
   pdf(file.path(directory, 'combinedKM.pdf', fsep = '\\'), onefile = FALSE)
@@ -109,11 +130,11 @@ runRiskStratifiedEstimation <- function(cohortMethodData,population, modelSettin
                                 plotsKM[[4]],
                                 nrow = 2))
   dev.off() # Needed?
-
+  
   pdf(file.path(directory, 'comparisonPlotHR.pdf', fsep = '\\'), onefile = FALSE)
   print(comparisonPlotHr(psList)$plot)
   dev.off()
-
+  
   nModels <- length(subFolder)
   dataARR <- data.frame(ARR = numeric(),
                         lower95 = numeric(),
@@ -124,7 +145,7 @@ runRiskStratifiedEstimation <- function(cohortMethodData,population, modelSettin
                        upper95 = numeric(),
                        riskStratum = numeric())
   for(i in 1:nModels){
-
+    
     directory <- file.path(save, subFolder[[i]], 'psList.rds', fsep = '\\')
     psList <- readRDS(directory)
     dataARR <- rbind(dataARR, absoluteRiskReductionDataFrame(psList = psList,
@@ -136,21 +157,21 @@ runRiskStratifiedEstimation <- function(cohortMethodData,population, modelSettin
                                                  useSW = TRUE,
                                                  truncatedWeights = TRUE,
                                                  truncationQuantiles = c(.01, .99)))
-
+    
   }
   dataARR$riskModel <- factor(rep(1:nModels, each = nStrata))
   levels(dataARR$riskModel) <- names(subFolder)
   dataHR$riskModel <- factor(rep(1:nModels, each = nStrata))
   levels(dataHR$riskModel) <- names(subFolder)
-
+  
   grid::grid.newpage()
   comparisonPlotGeneral(dataARR,
                         dataHR,
                         ylimARR = c(-.1, .1),
                         ylimHR = c(0, 1.5),
                         legend.position = c(.05, .1))
-
-
+  
+  
   pdf(file.path(mainFolder, 'comparisonPlotNew.pdf', fsep = '\\'), onefile = FALSE)
   print(comparisonPlotGeneral(dataARR,
                               dataHR,
