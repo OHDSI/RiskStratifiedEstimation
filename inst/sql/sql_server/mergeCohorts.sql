a7285a8ba1e8355707f79ec56326e378a7f18a5d
Alexandros Rekkas <a.rekkas@lumc.nl>
2019-02-28 14:05:15 +0100

    add user-defined cohort_definition_id

--- inst/sql/sql_server/mergeCohorts.sql
+++ inst/sql/sql_server/mergeCohorts.sql
@@ -5,9 +5,11 @@ File mergeCohorts.sql
 IF OBJECT_ID('@result_database_schema.@merged_cohorts', 'U') IS NOT NULL
 DROP TABLE @result_database_schema.@merged_cohorts;
 
-CREATE TYPE prijnbeek_CohortList AS TABLE (cohort_id int)
 
-SELECT *
+SELECT @target_cohort_id AS cohort_definition_id,
+  subject_id,
+  cohort_start_date,
+  cohort_end_date
 INTO @result_database_schema.@merged_cohorts
 FROM @cohort_database_schema.@cohort_table
-where cohort_definition_id IN (SELECT cohort_id FROM @cohort_ids);
+WHERE cohort_definition_id IN (@cohort_ids);
