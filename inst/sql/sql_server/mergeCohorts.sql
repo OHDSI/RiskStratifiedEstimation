3d814e26f29e547eb6f4e03aed51ad198fe81cfe
Alexandros Rekkas <a.rekkas@lumc.nl>
2019-02-28 13:34:44 +0100

    Create type to pass the list

--- inst/sql/sql_server/mergeCohorts.sql
+++ inst/sql/sql_server/mergeCohorts.sql
@@ -5,7 +5,9 @@ File mergeCohorts.sql
 IF OBJECT_ID('@result_database_schema.@merged_cohorts', 'U') IS NOT NULL
 DROP TABLE @result_database_schema.@merged_cohorts;
 
+CREATE TYPE prijnbeek_CohortList AS TABLE (cohort_id int)
+
 SELECT *
 INTO @result_database_schema.@merged_cohorts
 FROM @cohort_database_schema.@cohort_table
-where cohort_definition_id IN @cohort_ids;
+where cohort_definition_id IN (SELECT cohort_id FROM @cohort_ids);
