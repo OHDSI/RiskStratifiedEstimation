c602031b6987a389c8f9f64f2dc5ab67d46bd581
Alexandros Rekkas <a.rekkas@lumc.nl>
2019-02-28 13:16:43 +0100

    Drop table if it already exists

--- inst/sql/sql_server/mergeCohorts.sql
+++ inst/sql/sql_server/mergeCohorts.sql
@@ -2,6 +2,9 @@
 File mergeCohorts.sql
 ***********************************/
 
+IF OBJECT_ID('@result_database_schema.@merged_cohorts', 'U') IS NOT NULL
+DROP TABLE @result_database_schema.@merged_cohorts;
+
 SELECT *
 INTO @result_database_schema.@merged_cohorts
 FROM @cohort_database_schema.@cohort_table
