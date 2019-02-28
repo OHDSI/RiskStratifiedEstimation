02e9a4a63cd5087e560f98679e252a7f15bf635e
Alexandros Rekkas <a.rekkas@lumc.nl>
2019-02-28 16:14:23 +0100

    update sql files

--- inst/sql/sql_server/mergeCohorts.sql
+++ inst/sql/sql_server/mergeCohorts.sql
@@ -2,14 +2,14 @@
 File mergeCohorts.sql
 ***********************************/
 
-IF OBJECT_ID('@result_database_schema.@merged_cohorts', 'U') IS NOT NULL
-DROP TABLE @result_database_schema.@merged_cohorts;
+IF OBJECT_ID('@result_database_schema.@merged_cohort_table', 'U') IS NOT NULL
+DROP TABLE @result_database_schema.@merged_cohort_table;
 
 
 SELECT @target_cohort_id AS cohort_definition_id,
   subject_id,
   cohort_start_date,
   cohort_end_date
-INTO @result_database_schema.@merged_cohorts
+INTO @result_database_schema.@merged_cohort_table
 FROM @cohort_database_schema.@cohort_table
 WHERE cohort_definition_id IN (@cohort_ids);
